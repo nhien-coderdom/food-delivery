138a74950377374429617405898fe0793259b33e
nhien-coderdom <nguyenngocnhien251@gmail.com>
2025-11-08 00:45:49 +0700

    Fix: use APP_URL for Strapi HTTPS cookie

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+
+  // ✅ Dùng đúng biến HTTPS
+  url: env('APP_URL', 'https://foodie-0jpf.onrender.com'),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
